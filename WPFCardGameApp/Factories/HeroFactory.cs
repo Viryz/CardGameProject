9960c08bcc19d753432282163ddc8754f083e7f7
Viryz <viryz1998@gmail.com>
2018-10-06 13:22:37 +0300

    Добавьте файлы проекта.

---
+++ WPFCardGameApp/Factories/HeroFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCardGameApp.Factories
+{
+    abstract class HeroFactory
+    {
+        public abstract Models.Decks.Deck CreateDeck();
+    }
+}
---
+++ WPFCardGameApp/Models/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCardGameApp.Models
+{
+    class Hero
+    {
+        public Decks.Deck Deck { get; set; }
+        public int Magic { get; set; }
+        public List<Cards.Card> Board { get; set; }
+
+        public Hero(Factories.HeroFactory factory)
+        {
+
+        }
+
+    }
+}
